cf6f994d02ac898a7f975f9eef65e9491b1ae3bd
Daniel Samoylov <daniel.samoylov@icloud.com>
2025-10-29 19:34:25 +0100

    chore: initial project setup, dependencies, and configuration

---
+++ src/main/java/com/debugassistant/backend/DebugassistantApplication.java
@@ -0,0 +1,13 @@
+package com.debugassistant.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugassistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DebugassistantApplication.class, args);
+    }
+
+}
